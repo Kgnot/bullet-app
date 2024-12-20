834740b4b0c6ea6c7cb3128bce659b64139ef324
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-12-20 09:05:14 -0500

    Income Layout is already completed

--- src/ui/views/Income/Income.tsx
+++ src/ui/views/Income/Income.tsx
@@ -4,6 +4,29 @@ import './Income.css'
 export const Income = () => {
     return (
         <>
+            <section className="Income">
+                <div className="income-top">
+                    <div className="income-top-left child-default-comp  child-income">
+
+                    </div>
+                    <div className="income-top-right">
+                        <div className ="income-top-right-top child-default-comp child-income">
+
+                        </div>
+                        <div className ="income-top-right-bot child-default-comp child-income">
+
+                        </div>
+                    </div>
+                </div>
+                <div className="income-bot">
+                    <div className="income-bot-left child-default-comp child-income">
+
+                    </div>
+                    <div className="income-bot-right child-default-comp child-income">
+
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
