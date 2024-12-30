196a00d7120aa298a0330f2c5f43d65f4ea0c149
Henry Ricaurte Mora <hricaurtem@proton.me>
2024-12-29 20:16:50 -0500

    Finalizado mas o menos

--- src/ui/initSignViews/ProfileCreater/component/WalletsConfig/WalletsConfig.tsx
+++ src/ui/initSignViews/ProfileCreater/component/WalletsConfig/WalletsConfig.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import {useState} from "react";
 import "./WalletsConfig.css"; // Suponiendo que tengas un archivo de estilos
 
 export const WalletsConfig = () => {
     const [wallets, setWallets] = useState([
-        { name: "Efectivo",img:"./wallets/Efectivo.png", amount: 0 },
-        { name: "Nequi",img:"./wallets/Nequi.png", amount: 0 },
-        { name: "Uala",img:"./wallets/Uala.png", amount: 0 },
-        { name: "Lulo",img:"./wallets/lulobank.png", amount: 0 },
-        { name: "Daviplata",img:"./wallets/daviplata.png", amount: 0 },
+        {name: "Efectivo", img: "./wallets/Efectivo.png", amount: 0},
+        {name: "Nequi", img: "./wallets/Nequi.png", amount: 0},
+        {name: "Uala", img: "./wallets/Uala.png", amount: 0},
+        {name: "Lulo", img: "./wallets/lulobank.png", amount: 0},
+        {name: "Daviplata", img: "./wallets/daviplata.png", amount: 0},
     ]);
 
     const handleSubmit = () => {
         setWallets(wallets);
     }
+
+    // para manejar el input
+    const handleAmountChange = (index: number, newAmount: string) => {
+        const updatedWallets = wallets.map((wallet, i) =>
+            i === index
+                ? {...wallet, amount: parseInt(newAmount) || 0}
+                : wallet
+        );
+        setWallets(updatedWallets);
+    };
+
+
     return (
         <div className="walletsConfig" onSubmit={handleSubmit}>
             {/*no mas para quitar el error pero debo cambiar lo de arriba*/}
@@ -28,6 +40,9 @@ export const WalletsConfig = () => {
                                 type="number"
                                 value={wallet.amount}
                                 placeholder={`Saldo en ${wallet.name}`}
+                                onChange={(e) =>
+                                    handleAmountChange(index, e.target.value)
+                                }
                             />
                         </div>
 
